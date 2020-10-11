2eecaee0e382cd9cb96d7c7afcb7a05df4a9cf77
Duncan Gibson <legowerewolf@gmail.com>
2020-10-10 17:54:48 -0700

    Clean up typings and names a little.

--- classic-api.ts
+++ classic-api.ts
@@ -4,9 +4,8 @@ type Writer<T> = ((data: T) => void) | { write: (data: T) => void };
 const getWriteFunc = <T>(writer: Writer<T>) =>
 	"write" in writer ? writer.write.bind(writer) : writer;
 
-function makeObjectCallable<F extends Function, O>(obj: O, func: F) {
-	return Object.assign(func, obj) as F & O;
-}
+const enrichFunction = <F extends Function, O>(func: F, obj: O): F & O =>
+	Object.assign(func, obj);
 
 type Ratlogger = ((
 	message: Stringable,
@@ -17,14 +16,10 @@ type Ratlogger = ((
 const generateRatlogger = (
 	writer: Writer<RatlogData>,
 	...tags: Stringable[]
-) => {
+): Ratlogger => {
 	let originalTags = tags;
 
-	return makeObjectCallable(
-		{
-			tag: (...tags: Stringable[]): Ratlogger =>
-				generateRatlogger(writer, ...originalTags.concat(tags)),
-		},
+	return enrichFunction(
 		(
 			message: Stringable,
 			fields?: RatlogData["fields"],
@@ -35,21 +30,25 @@ const generateRatlogger = (
 				fields,
 				tags: originalTags.concat(tags),
 			});
+		},
+		{
+			tag: (...tags: Stringable[]): Ratlogger =>
+				generateRatlogger(writer, ...originalTags.concat(tags)),
 		}
-	) as Ratlogger;
+	);
 };
 
 const ratlog = (() => {
-	return makeObjectCallable(
-		{
-			logger: generateRatlogger,
-			stringify: Ratlog.format,
-		},
+	return enrichFunction(
 		(writer: Writer<string>, ...tags: Stringable[]) =>
 			generateRatlogger(
 				(data: RatlogData) => getWriteFunc(writer)(Ratlog.format(data)),
 				...tags
-			)
+			),
+		{
+			logger: generateRatlogger,
+			stringify: Ratlog.format,
+		}
 	);
 })();
 
