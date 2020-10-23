74294d6490f1bb1a277eaa811e1cb0e67d34fdb3
Duncan Gibson <legowerewolf@gmail.com>
2020-10-23 14:43:10 -0700

    Add more notes to classic-api.ts

--- classic-api.ts
+++ classic-api.ts
@@ -4,17 +4,30 @@ type Writer<T> = ((data: T) => void) | { write: (data: T) => void };
 const getWriteFunc = <T>(writer: Writer<T>) =>
 	"write" in writer ? writer.write.bind(writer) : writer;
 
+/**
+ * Take a function and an object and produce a function with properties.
+ * @param func the function to attach properties to
+ * @param obj an object containing properties to copy to the function
+ */
 const enrichFunction = <F extends Function, O>(func: F, obj: O): F & O =>
 	Object.assign(func, obj);
 
-type Ratlogger = ((
+/**
+ * A `Ratlogger` is a function that takes the components of a `RatlogData` and
+ * does something with it. It also exposes a `tag` property-function which produces
+ * an identical `Ratlogger` with the added tags.
+ */
+export type Ratlogger = ((
 	message: Stringable,
 	fields?: RatlogData["fields"],
 	...tags: Stringable[]
 ) => void) & { tag: (...tags: Stringable[]) => Ratlogger };
 
 const generateRatlogger = (
+	/** a writable stream or function that can take RatlogData */
 	writer: Writer<RatlogData>,
+
+	/** pre-set tags */
 	...tags: Stringable[]
 ): Ratlogger => {
 	let originalTags = tags;
@@ -32,7 +45,7 @@ const generateRatlogger = (
 			});
 		},
 		{
-			tag: (...tags: Stringable[]): Ratlogger =>
+			tag: (/** pre-set tags */ ...tags: Stringable[]): Ratlogger =>
 				generateRatlogger(writer, ...originalTags.concat(tags)),
 		}
 	);
@@ -40,13 +53,21 @@ const generateRatlogger = (
 
 const ratlog = (() => {
 	return enrichFunction(
+		/**
+		 * @param writer a writable stream or function
+		 * @param tags pre-set tags
+		 * @returns a logging function bound to the writer
+		 */
 		(writer: Writer<string>, ...tags: Stringable[]) =>
 			generateRatlogger(
 				(data: RatlogData) => getWriteFunc(writer)(Ratlog.format(data)),
 				...tags
 			),
 		{
+			/** Constructor for more customizable logger instances. */
 			logger: generateRatlogger,
+
+			/** Exposure of the core Ratlog string formatter. */
 			stringify: Ratlog.format,
 		}
 	);
